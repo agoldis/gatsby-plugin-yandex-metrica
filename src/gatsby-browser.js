726e2c45cbd2f5e92a30539a523f04212b9ffda8
mellowjoey <mellowjoey@yandex.com>
2019-03-09 01:23:55 +0600

    Initial commit

---
+++ src/gatsby-browser.js
@@ -0,0 +1,30 @@
+exports.onRouteUpdate = function ({ location }) {
+  if (process.env.NODE_ENV === `production` && typeof ym === `function`) {
+    if (
+      location &&
+      typeof window.ym !== `undefined`
+    ) {
+      return
+    }
+
+    // wrap inside a timeout to make sure react-helmet is done with it's changes (https://github.com/gatsbyjs/gatsby/issues/9139)
+    // reactHelmet is using requestAnimationFrame so we should use it too: https://github.com/nfl/react-helmet/blob/5.2.0/src/HelmetUtils.js#L296-L299
+    const sendPageView = () => {
+      window.ym(
+        `hit`,
+        location
+          ? location.pathname + location.search + location.hash
+          : undefined
+      )
+    }
+
+    if (`requestAnimationFrame` in window) {
+      requestAnimationFrame(() => {
+        requestAnimationFrame(sendPageView)
+      })
+    } else {
+      // simulate 2 rAF calls
+      setTimeout(sendPageView, 32)
+    }
+  }
+}
---
+++ src/gatsby-ssr.js
@@ -0,0 +1,41 @@
+import React from "react"
+
+const knownOptions = {
+  clickmap: `boolean`,
+  trackLinks: `boolean`,
+  accurateTrackBounce: `boolean`,
+  trackHash: `boolean`,
+  webvisor: `boolean`,
+}
+
+exports.onRenderBody = (
+  { setPostBodyComponents },
+  pluginOptions
+) => {
+  if (process.env.NODE_ENV === `production`) {
+    let ymCreateOptions = {}
+    for (const option in knownOptions) {
+      if (typeof pluginOptions[option] === knownOptions[option]) {
+        ymCreateOptions[option] = pluginOptions[option]
+      }
+    }
+
+    return setPostBodyComponents([
+      <script
+        key={`gatsby-plugin-yandex-metrica`}
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+            ym(${pluginOptions.trackingId}, "init", ${JSON.stringify(ymCreateOptions)});
+          `
+        }}
+      />,
+      <noscript><div><img src={`https://mc.yandex.ru/watch/${pluginOptions.trackingId}`} style={{ position: absolute, left: "-9999px" }} alt="" /></div></noscript>
+    ])
+  }
+
+  return null
+}

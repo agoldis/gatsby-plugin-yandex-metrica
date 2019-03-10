9932d17263fe24065ec8318657792a20118cd07b
mellowjoey <mellowjoey@yandex.com>
2019-03-10 14:02:18 +0600

    Quick test

--- src/gatsby-browser.js
+++ src/gatsby-browser.js
@@ -2,7 +2,8 @@ exports.onRouteUpdate = function ({ location }) {
   if (process.env.NODE_ENV === `production` && typeof ym === `function`) {
     if (
       location &&
-      typeof window.ym !== `undefined`
+      typeof window.ym !== `undefined` &&
+      typeof window.ym.hit === `function`
     ) {
       return
     }
@@ -10,8 +11,7 @@ exports.onRouteUpdate = function ({ location }) {
     // wrap inside a timeout to make sure react-helmet is done with it's changes (https://github.com/gatsbyjs/gatsby/issues/9139)
     // reactHelmet is using requestAnimationFrame so we should use it too: https://github.com/nfl/react-helmet/blob/5.2.0/src/HelmetUtils.js#L296-L299
     const sendPageView = () => {
-      window.ym(
-        `hit`,
+      window.ym.hit(
         location
           ? location.pathname + location.search + location.hash
           : undefined

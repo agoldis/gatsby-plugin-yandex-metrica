f72d61d91f30ac68a483f472e0c19e6dd0711a2d
Andrew Goldis <agoldis@gmail.com>
2020-03-04 23:27:07 -0800

    Stop using depricated ym.hit

--- src/gatsby-browser.js
+++ src/gatsby-browser.js
@@ -1,9 +1,8 @@
-exports.onRouteUpdate = function ({ location }) {
+exports.onRouteUpdate = function ({ location }, {trackingId}) {
   if (process.env.NODE_ENV === `production` && typeof ym === `function`) {
     if (
       location &&
-      typeof window.ym !== `undefined` &&
-      typeof window.ym.hit === `function`
+      typeof window.ym !== `undefined`
     ) {
       return
     }
@@ -11,11 +10,11 @@ exports.onRouteUpdate = function ({ location }) {
     // wrap inside a timeout to make sure react-helmet is done with it's changes (https://github.com/gatsbyjs/gatsby/issues/9139)
     // reactHelmet is using requestAnimationFrame so we should use it too: https://github.com/nfl/react-helmet/blob/5.2.0/src/HelmetUtils.js#L296-L299
     const sendPageView = () => {
-      window.ym.hit(
+      window.ym('hit', trackingId, (
         location
           ? location.pathname + location.search + location.hash
           : undefined
-      )
+      ))
     }
 
     if (`requestAnimationFrame` in window) {
